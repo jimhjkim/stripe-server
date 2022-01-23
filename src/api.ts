1e685819448f32538aa096fd107d7d61605b7023
Jim Kim <jim.hyunjin.kim@gmail.com>
2022-01-22 23:20:46 -0500

    Setup stripe checkout on the server

--- src/api.ts
+++ src/api.ts
@@ -4,6 +4,7 @@ export const app = express();
 app.use(express.json());
 
 import cors from "cors";
+import { createStripeCheckoutSession } from "./checkout";
 
 app.use(cors({ origin: true }));
 
@@ -11,3 +12,22 @@ app.post("/test", (req: Request, res: Response) => {
   const amount = req.body.amount;
   res.status(200).send({ with_tax: amount * 7 });
 });
+
+/**
+ * Catch async errors when awaiting promises
+ */
+function runAsync(callback: Function) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    callback(req, res, next).catch(next);
+  };
+}
+
+/**
+ * Checkouts
+ */
+app.post(
+  "/checkouts/",
+  runAsync(async ({ body }: Request, res: Response) => {
+    res.send(await createStripeCheckoutSession(body.line_items));
+  })
+);
